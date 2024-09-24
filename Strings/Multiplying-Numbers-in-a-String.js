6d86406f8c2ba09fc0b67774b4594793c1849ba7
Carlos M <53409854+Cema2019@users.noreply.github.com>
2024-09-23 18:43:13 -0600

    Create Multiplying-Numbers-in-a-String.js

---
+++ Strings/Multiplying-Numbers-in-a-String.js
@@ -0,0 +1,14 @@
+/*Given a string of numbers separated by a comma and space, return the product of the numbers.
+
+Examples
+multiplyNums("2, 3") ➞ 6
+
+multiplyNums("1, 2, 3, 4") ➞ 24
+
+multiplyNums("54, 75, 453, 0") ➞ 0
+
+multiplyNums("10, -2") ➞ -20*/
+
+function multiplyNums(nums) {
+	return nums.split(", ").map(Number).reduce((a, b) => a * b, 1);
+}
